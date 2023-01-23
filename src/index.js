961bb8f6296f6ce1259318035f71fedf96c8f8ec
eSuardOdin <esuardodin@gmail.com>
2023-01-23 18:35:03 +0100

    Adding a few more webpack plugins

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from "lodash";
+import './style.css';
+import printMe from "./print";
+
 
 function component() {
+    const btn = document.createElement('button');
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console.'
+    element.appendChild(btn);
+    btn.onclick =  () => printMe();
     element.classList.add('test');
     return element;
 }
