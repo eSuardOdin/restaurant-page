ac32794ef9aa4f83d5d5f38f6c0b123997780208
eSuardOdin <esuardodin@gmail.com>
2023-01-24 08:56:09 +0100

    Added few classes in page content

--- src/about.js
+++ src/about.js
@@ -2,8 +2,18 @@ export default function getAboutpage() {
     const pageTitle = document.createElement('div');
     pageTitle.classList.add('page-title');
     pageTitle.innerHTML = 'About Us';
+
+    const pageContent = document.createElement('div');
+    pageContent.classList.add('page-content');
+    pageContent.innerHTML = `
+    <p>Lorem necessitatibus et incidunt fuga. Delectus quas eum voluptates quibusdam odio architecto adipisci nulla consectetur corrupti temporibus doloremque inventore officiis quae nihil, qui non reiciendis necessitatibus. Rem optio soluta animi inventore nemo repellendus dicta rerum ipsum deserunt, nam quae, dolore illum adipisci ex, culpa quidem blanditiis. Doloremque nihil ipsum deleniti obcaecati!</p>
+    `;
+
+
+
     const element = document.createElement('div');
     element.appendChild(pageTitle);
+    element.appendChild(pageContent);
     element.classList.add('page-element');
     return element;
 }
--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,19 @@
-export default function getContactpage() {
+export default function getAboutpage() {
+    const pageTitle = document.createElement('div');
+    pageTitle.classList.add('page-title');
+    pageTitle.innerHTML = 'Contact';
+
+    const pageContent = document.createElement('div');
+    pageContent.classList.add('page-content');
+    pageContent.innerHTML = `
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam minus fugit tempore quae repudiandae repellendus cupiditate labore reiciendis, sunt at atque beatae quo facilis odit deserunt. Ex repellendus dolorum pariatur? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias eveniet ratione ad saepe sequi explicabo dignissimos ex quae ut perferendis nemo, voluptatum delectus beatae! Dolor dolorem iste animi quos aperiam dignissimos in, qui doloremque hic esse excepturi ratione eveniet natus illum quis quod aspernatur ipsum deleniti obcaecati!</p>
+    `;
+
+
+
     const element = document.createElement('div');
+    element.appendChild(pageTitle);
+    element.appendChild(pageContent);
     element.classList.add('page-element');
-    element.innerHTML = 'Contact';
     return element;
 }
