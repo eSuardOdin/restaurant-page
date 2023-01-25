6eef37663109f57ac80b7f10936db294dadf19de
eSuardOdin <esuardodin@gmail.com>
2023-01-25 08:52:26 +0100

    Fixed img, needed to import them, now I need to resize accordingly

--- src/about.js
+++ src/about.js
@@ -1,4 +1,5 @@
 import createArticle from "./createArticle";
+import coffeImg from './images/small/coffee-grains.jpg'
 
 export default function getAboutpage() {
     const pageTitle = document.createElement('div');
@@ -11,7 +12,9 @@ export default function getAboutpage() {
     // <p>Lorem necessitatibus et incidunt fuga. Delectus quas eum voluptates quibusdam odio architecto adipisci nulla consectetur corrupti temporibus doloremque inventore officiis quae nihil, qui non reiciendis necessitatibus. Rem optio soluta animi inventore nemo repellendus dicta rerum ipsum deserunt, nam quae, dolore illum adipisci ex, culpa quidem blanditiis. Doloremque nihil ipsum deleniti obcaecati!</p>
     // `;
 
-    pageContent.appendChild(createArticle('/home/wan/repos/restaurant-page/src/images/small/coffee-grains.jpg', 'Our coffee is not responsible for polar bear deaths', 'Eco responsible coffee', true));
+    pageContent.appendChild(createArticle(coffeImg, 'Our coffee is not responsible for polar bear deaths', 'Eco responsible coffee', false));
+
+    pageContent.appendChild(createArticle('/home/wan/repos/restaurant-page/src/images/small/coffee-grains.jpg', 'Lorem lorem lorem tg lol', 'Some blabla', true));
 
     const element = document.createElement('div');
     element.appendChild(pageTitle);
